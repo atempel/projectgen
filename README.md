4013e2967545bc8bbcabaf87eb8d4ef6748c1c21
atempel <alexandre.tempel@greenz.com.br>
2019-01-17 16:33:01 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# This project is under construction :)
+
+If you're willing to help, you can contact me here at GitHub via the [contribute issue](https://github.com/atempel/projectgen/issues/1).
+
+Thanks!
+
---
+++ _javascript/main.js
@@ -0,0 +1,36 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//   console.log('Hello Bulma!');
+// });
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    min: 1,
+    max: 10,
+    randomNumber: 1,
+    generating: false,
+    pGenerated: "Your project will be generated here"
+  },
+  created: function () {
+    this.generateNumber()
+  },
+  methods: {
+    loadNum: function () {
+      this.min = 1;
+      this.max = 10;
+      this.generateNumber();
+    },
+    getInput: function () {
+      if(this.min > this.max) {
+        [this.min, this.max] = [this.max, this.min]
+      }
+      this.generateNumber()
+    },
+    generateProject: function() {
+    	// WIP
+    },
+    generateNumber: function () {
+      this.randomNumber = Math.floor(Math.random()*(this.max-this.min+1)+this.min);
+    }
+  }
+});
